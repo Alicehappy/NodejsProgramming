f37a612ed2165dc13346bef68237c143addef905
Alice <alicezou05@gmail.com>
2017-10-23 09:46:38 -0400

    WebSocket: Add server side websocket receive message from the websocket on the browser side

--- app.js
+++ app.js
@@ -25,10 +25,31 @@ const serveIndex = (req, resp) => {
   resp.end();
 };
 
+/*
+// Send and receiving text data
 wss.on('connection', function connection(ws){
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
   });
 
   ws.send("something");
+});
+*/
+
+
+// Broadcast to all.
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    // Receiving the box which is checked
+        console.log(data);
+
+  });
 });
